ed506425a29718be8653f20dfdbf8b954ac53201
kkopp <kristinakopp@Kristinas-MacBook-Pro.local>
2018-12-24 04:44:07 -0700

    fixed links in api file

--- app/data/friends.js
+++ app/data/friends.js
@@ -1,12 +1,12 @@
 var friends = [
   {
     name: 'Summer',
-    photo: 'https://loremflickr.com/320/240"',
+    photo: 'https://loremflickr.com/320/240/dog',
     scores: [5, 1, 4, 4, 5, 1, 2, 5, 4, 1],
   },
   {
     name: 'Artemis',
-    photo: 'https://loremflickr.com/320/240"',
+    photo: 'https://loremflickr.com/320/240/dog',
     scores: [1, 5, 2, 2, 1, 5, 4, 1, 2, 5],
   },
 ];
