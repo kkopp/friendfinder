150c3faf9f0db95dfd85c0edee0c89b23a0306b5
kkopp <kristinakopp@Kristinas-MacBook-Pro.local>
2019-01-02 19:32:16 -0700

    trying to get js to work

--- app/routing/apiRoutes.js
+++ app/routing/apiRoutes.js
@@ -1,29 +1,42 @@
-var path = require('path');
+var path = require ('path');
 
-var friends = require('../data/friends.js');
+var friends = require ('../data/friends.js');
 
 module.exports = function (app) {
   app.get ('/api/friends', function (req, res) {
-    res.json(friends);
+    res.json (friends);
   });
 
   //other routes..
 
   app.post ('/api/friends', function (req, res) {
-
     var newFriend = req.body;
-
-    newFriend.routeName = newFriend.name
-      .replace (/\s+/g, '');
-
-    newFriend.routeName = newFriend.photo;
-    newFriend.routeName = newFriend.scores;
-
     console.log (newFriend);
+
+    var scores = newFriend.scores;
+
+    var newFriendName = '';
+    var newFriendPhoto = '';
+    var totalDiff = 1000;
+
+    for (var i = 0; i < friends.length; i++) {
+      var diff = 0;
+      for (var j = 0; j < scores.length; j++) {
+        diff += Math.abs (friends[i].score[j] - scores[j]);
+      }
+    }
+
+    if (diff < totalDiff) {
+      console.log('Closest match found = ' + diff);
+      console.log('Friend name = ' + friends[i].name);
+      console.log('Friend image = ' + friends[i].photo);
+      totalDiff = diff;
+      newFriendName = friends[i].name;
+      newFriendPhoto = friends[i].image;
+    }
 
     friends.push (newFriend);
 
-    res.json (newFriend);
+    res.json ({newFriendName: newFriendName, newFriendPhoto: newFriendPhoto});
   });
-
 };
