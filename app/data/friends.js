1654a0a886b045061e7341310808d46d5fdc382f
kkopp <kristinakopp@Kristinas-MacBook-Pro.local>
2019-01-06 20:05:16 -0700

    added some styling to the modal

--- app/data/friends.js
+++ app/data/friends.js
@@ -1,13 +1,33 @@
 var friends = [
   {
     name: 'Summer',
-    photo: 'https://loremflickr.com/320/240/dog',
+    photo: 'https://s3.amazonaws.com/filestore.rescuegroups.org/6874/pictures/animals/12960/12960852/56721010_480x442.jpg',
     scores: [5, 1, 4, 4, 5, 1, 2, 5, 4, 1],
   },
   {
     name: 'Artemis',
-    photo: 'https://loremflickr.com/320/240/dog',
+    photo: 'https://img-aws.ehowcdn.com/600x600p/photos.demandstudios.com/59/46/fotolia_3705190_XS.jpg',
     scores: [1, 5, 2, 2, 1, 5, 4, 1, 2, 5],
+  },
+  {
+    name: 'Captain Crunch',
+    photo: 'https://www.petmeds.org/wp-content/uploads/2013/01/Strudel.jpg',
+    scores: [4, 1, 5, 3, 1, 2, 1, 4, 3, 5],
+  },
+  {
+    name: 'Mollie',
+    photo: 'https://i.pinimg.com/originals/8b/33/0c/8b330cb1e7f88202b73eaf4d5b3429cb.jpg',
+    scores: [2, 1, 3, 1, 5, 2, 1, 3, 4, 1],
+  },
+  {
+    name: 'Koby',
+    photo: 'https://www.petmd.com/sites/default/files/breedopedia/shetland%20bud.jpg',
+    scores: [3, 3, 5, 5, 3, 2, 4, 5, 3, 3],
+  },
+  {
+    name: 'Erita',
+    photo: 'https://www.ckvets.com/wp-content/uploads/2017/06/Tucker-Webpage.jpg',
+    scores: [1, 3, 5, 4, 4, 3, 4, 5, 5, 5],
   },
 ];
 
